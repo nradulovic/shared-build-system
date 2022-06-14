760b1d937eac60f76ca26641d60dc9691bfdce3a
Nenad Radulovic <nenad.b.radulovic@gmail.com>
2022-06-14 15:58:06 +0200

    feat: generate a report for test
    Signed-off-by: Nenad Radulovic <nenad.b.radulovic@gmail.com>

--- target_test.mk
+++ target_test.mk
@@ -3,4 +3,6 @@ include $(WS)/build/sbs/target_application.mk
 
 .PHONY: test
 test: $(PROJECT_ELF)
-	$<
+	$< > $(WS)/build/generated/report_$(PROJECT_NAME)
+	@echo Report in: $(WS)/build/generated/report_$(PROJECT_NAME)
+	@cat $(WS)/build/generated/report_$(PROJECT_NAME)
